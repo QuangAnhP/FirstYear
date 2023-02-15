b5662ffd9ce5e4499bd674f0b5104cd91af2347f
QuangAnhP <quanganh.pham31072004@gmail.com>
2023-02-14 17:01:39 -0800

    W5

--- Schoolworks/W6 - Strings/Theory.py
+++ Schoolworks/W6 - Strings/Theory.py
@@ -5,8 +5,13 @@ message = 'Hello World'
 print(message[0])
 
 print(len(message))
+print()
+    
+    # Can also iterate directly to string:
+for i in message:
+    print(i)
 
-# %% Reverse
+# %% Reversing a string
     
 for i in range(len(message) - 1, -1, -1):
     print(message[i])
@@ -35,4 +40,71 @@ def isPalindrome(string):
 
 isPalindrome(input("Enter a string: "))
 
-# %% 
+# %% search in string:
+
+def isFound(s, c):
+    for i in range(len(s)):
+        if c == s[i]:
+            return True
+    return False
+
+isFound('abcdefgh', 'c')
+
+    # Expand: 
+    # if c in s: ...
+    # find(): find the location
+
+# %% Remove duplicates:
+
+def removeDupe(string):
+    new_string = ''
+    for i in range(len(string)):
+        if isFound(new_string, string[i]) is False:
+            new_string = new_string + string[i]
+    return new_string
+
+removeDupe("aadabaadbdca")
+
+# %% Excercise: Replace
+
+def replaceStr(s, old, new):
+    result = ''
+    i = 0
+    while i < (len(s)):
+        
+        if s[i] == old[0]:
+            temp = s[i : i+len(old)]
+        
+            if temp == old:
+                result += new
+                i += len(old)    
+        
+        else:
+            result += s[i]
+            i += 1
+    return result
+
+
+replaceStr("To be or not to be", "be", "?")
+
+# %% Comparing strings using ASCII:
+
+'''
+Dictionary order: 
+ex. "abc" and "abd"
+Compare a with a, then b with b, then c with d -> "abc" < "abd"
+
+"a" < "A"
+"abc" < "abcde"
+"apple" > "ape"
+"apple" < "Ape"
+"Apple" > "ape"
+'''
+
+def isEqual(a, b):
+    for i in range(max(len(a), len(b))):    # or check for different lenght outside
+        if (len(a) != len(b)) or (a[i] != b[i]):
+            return False
+    return True
+
+isEqual("abcde", "abcde")
